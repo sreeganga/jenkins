e336571b51b6ca1cc705de408394e0ad9567bd32
user <user@DESKTOP-6FJKV3E>
2024-03-13 14:05:15 +0530

    jenkin class

---
+++ jenkinsproject/src/main/java/jenkinspack/ClassA.java
@@ -0,0 +1,14 @@
+package jenkinspack;
+
+public class ClassA {
+	public void show()
+	{
+		System.out.println("hai");
+	}
+public static void main(String args[])
+{
+	ClassA obj=new ClassA();
+	obj.show();
+
+}
+}
---
+++ jenkinsproject/src/main/java/jenkinspack/ClassB.java
@@ -0,0 +1,17 @@
+package jenkinspack;
+
+public class ClassB {
+	public void show()
+	{
+		String name="sree";
+		int age= 27;
+		System.out.println(name+"  "+ age);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassB obj= new ClassB();
+		obj.show();
+	}
+
+}
